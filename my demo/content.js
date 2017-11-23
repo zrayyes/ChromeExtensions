cccbb567e36d930714872c7634c5c7b0841ece90
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-23 23:37:08 +0100

    Change colors of li under contributions

--- my demo/content.js
+++ my demo/content.js
@@ -1,19 +1,16 @@
 const colors = {
-    "#ebedf0" : "#ebedf0",
-    "#c6e48b" : "#1EFF00",
-    "#7bc96f" : "#0070ff",
-    "#239a3b" : "#a335ee",
-    "#196127" : "#ff8000"
+    // "#ebedf0" : "#ebedf0",
+    "#c6e48b" : "#1EFF00",  // l1
+    "#7bc96f" : "#0070ff",  // l2
+    "#239a3b" : "#a335ee",  // l3
+    "#196127" : "#ff8000"   // l4
 };
 
-const wow_colors = [
-    "#ebedf0",
-    "#1EFF00",
-    "#0070ff",
-    "#a335ee",
-    "#ff8000"
-];
-
 for (let key in colors) {
+    // rect blocks in contributions
     $(`rect[fill='${key}']`).attr("fill",colors[key]);
+
+    // li blocks under contributions
+    $(`li[style='background-color: ${key}']`)
+    .attr("style",`background-color: ${colors[key]}`);
 }
