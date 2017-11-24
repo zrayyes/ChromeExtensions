ee89d49ed05b21e2adaaab50e41f97e0ddae1073
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-24 16:04:56 +0100

    Changed loop to function

--- my demo/content.js
+++ my demo/content.js
@@ -6,16 +6,20 @@ const colors = {
     "#196127" : "#ff8000"   // l4
 };
 
-for (let key in colors){
-    // Change color of list elements under contributions
-    document.querySelector(`li[style='background-color: ${key}']`)
-    .setAttribute("style",`background-color: ${colors[key]}`);
+function changeColors() {
+    for (let key in colors){
+        // Change color of list elements under contributions
+        document.querySelector(`li[style='background-color: ${key}']`)
+        .setAttribute("style",`background-color: ${colors[key]}`);
 
-    // return all rect elements as NodeList
-    let arr = document.querySelectorAll(`rect[fill='${key}']`);
+        // return all rect elements as NodeList
+        let arr = document.querySelectorAll(`rect[fill='${key}']`);
 
-    // loop over rect elements changing colors
-    for (var div of arr) {
-        div.setAttribute("fill",colors[key]);
-    };
+        // loop over rect elements changing colors
+        for (var div of arr) {
+            div.setAttribute("fill",colors[key]);
+        };
+    }
 }
+
+changeColors();
