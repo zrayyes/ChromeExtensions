2b044e61fc2bfb56f88431c6b86fce4cc13b472b
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-26 21:48:39 +0100

    Able to change text on page now

--- UnitsConverter/content.js
+++ UnitsConverter/content.js
@@ -1,7 +1,20 @@
-document.addEventListener('mouseup',function(event)
-{
-    var sel = window.getSelection().toString();
-    if(sel.length) {
-        console.log(sel);
+function changeTextTo(Text) {
+    sel = window.getSelection();
+    selectedText = sel.toString()
+    selectedText = selectedText.replace(/ /g,'');
+
+    if (selectedText.length){
+        if (sel.rangeCount && sel.getRangeAt) {
+            range = sel.getRangeAt(0);
+        }
+        document.designMode = "on";
+
+        document.execCommand('insertText', false, ` ${Text} `);
+
+        document.designMode = "off";
     }
+}
+
+document.addEventListener('mouseup',function(event) {
+    changeTextTo('Hello');
 })
